b2ab569d1a55139a1db14728b7b659ec599b7a76
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-08-08 14:51:01 +0200

    live

--- README.md
+++ README.md
@@ -21,3 +21,7 @@ It correctly bundles React in production mode and optimizes the build for the be
 The build is minified and the filenames include the hashes.\
 Your app is ready to be deployed!
 
+### Deploy State 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/d4863f86-906a-4da0-93cb-a4ae6e1e0878/deploy-status)](https://app.netlify.com/sites/astounding-valkyrie-a88601/deploys)
+### Live Site 
+[Anecdotes](https://astounding-valkyrie-a88601.netlify.app/)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 // بسم الله الرحمن الرحيم
 import { useState } from 'react'
 const getRandomInt =(max)=>{
-  return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max * .5);
 }
-const Display = () => {
-  
-}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -16,19 +14,30 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-  const [points, setPoint] = useState({})
+  const [points, setPoint] = useState([])
+
+
   const [selected, setSelected] = useState(getRandomInt(anecdotes.length))
+  
   const changeClick=()=>{
     setSelected(getRandomInt(anecdotes.length))
   }
   const voteClick=()=>{
     setPoint({...points, [selected]:points[selected]?points[selected]+1:1})
-
-    console.log(points)
-    
+    mostVotes()
   }
   const votes= points[selected]?points[selected]:0
- 
+ const mostVotes =()=>{
+    let max = 0
+    let maxIndex = 0
+    for(let i=0;i<anecdotes.length;i++){
+      if(points[i]>max){
+        max = points[i]
+        maxIndex = i
+      }
+    }
+    return max >0 ? maxIndex:null
+ }
   
   return (
     <div>
@@ -36,10 +45,11 @@ const App = () => {
       <h2>Anecdote of the day</h2>
       <p>{anecdotes[selected]}</p>
       <p>has {votes} votes</p>
-      <button onClick={changeClick}>Next anecdote</button>
+      <button onClick={changeClick}>Random anecdote</button>
       <button onClick={voteClick}>vote</button>
       <h2>Anecdote with the most votes</h2>
-      
+      <p>{anecdotes[mostVotes()]}</p>
+      <footer><a href="https://github.com/naderkamelaponar/fs-anecdotes">Repo</a> </footer>
     </div>
   )
 }
